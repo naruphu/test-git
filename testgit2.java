9451c5ed5c954515c4be6ef6ea4a11198938147a
NARUPHU <naruphu7@gmail.com>
2026-04-16 12:10:32 +0700

    add testgit4

--- testgit2.java
+++ testgit2.java
@@ -1,5 +1,5 @@
 class testgit2{
     public static void main(String[] args) {
-        System.out.println("nguyen trong phu dep trai");
+        System.out.println("nguyen trong phu dep trai se tro nen thanh cong va tu tin vao ban than");
     }
 }
