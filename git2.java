6ebbba93e256ee96cd9cdb18e739664c1d3789b5
NARUPHU <naruphu7@gmail.com>
2026-04-16 11:58:29 +0700

    sua lan 4

--- git2.java
+++ git2.java
@@ -1,5 +1,5 @@
 class git2{
     public static void main(String[] args) {
-        System.out.println("test 2 hihihihihi sua loi ne va  sua lan thu2 ne kkk");
+        System.out.println("test 2 hihihihihi sua loi ne va  sua lan thu2 ne kkk abcd");
     }
 }
